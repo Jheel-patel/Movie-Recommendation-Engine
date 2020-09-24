5d55daafdec39c5796e807f0b8a0974d117ebccf
Jheel Patel <jheel.jp@gmail.com>
2020-09-24 17:20:29 -0400

    Final touch-up

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask,request,jsonify
+from flask import Flask, render_template, request
 from flask_cors import CORS
 import recommendation
 
 
 app = Flask(__name__)
 CORS(app) 
-        
-@app.route('/movie', methods=['GET'])
-def recommend_movies():
-    res = recommendation.results(request.args.get('title'))
-    return jsonify(res)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/recommend")
+def recommend():
+    movie = request.args.get('movie')
+    r = recommendation.results(movie)
+    movie = movie.upper()
+    if type(r)==type('string'):
+        return render_template('recommend.html',movie=movie,r=r,t='s')
+    else:
+        return render_template('recommend.html',movie=movie,r=r,t='l')
 
 if __name__=='__main__':
     app.run(port = 5000, debug = True)
